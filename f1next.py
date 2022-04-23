9673bb73139f712379b96d8777f6ff1a088b78c3
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-04-23 15:33:56 -0300

    Turned everything into a function

--- f1next.py
+++ f1next.py
@@ -8,69 +8,89 @@ import click
 import requests_cache
 from dateutil import tz
 
-cache_dir = appdirs.user_cache_dir("f1next", "f1next")
-cache_file = "race_cache"
-cache_path = Path(cache_dir, cache_file)
 
-request = requests_cache.CachedSession(cache_path)
-data = request.get("https://ergast.com/api/f1/current/next.json").json()
+def download_gp_info():
+    cache_dir = appdirs.user_cache_dir("f1next", "f1next")
+    cache_file = "race_cache"
+    cache_path = Path(cache_dir, cache_file)
 
-next_race = data["MRData"]["RaceTable"]["Races"][0]
+    request = requests_cache.CachedSession(cache_path)
+    return request.get("https://ergast.com/api/f1/current/next.json").json()
 
-race_name = next_race["raceName"]
-race_time = next_race["time"][:-1]
-race_date = next_race["date"]
+
+def parse_gp_info(json):
+    gp_info = {}
+    gp_base_info = json["MRData"]["RaceTable"]["Races"][0]
+
+    gp_info["name"] = gp_base_info["raceName"]
+    gp_info["country"] = gp_base_info["Circuit"]["Location"]["country"]
+
+    gp_info["race"] = get_event_time(gp_base_info["date"], gp_base_info["time"])
+
+    gp_info["quali"] = get_event_time(
+        gp_base_info["Qualifying"]["date"], gp_base_info["Qualifying"]["time"]
+    )
+
+    gp_info["practice1"] = get_event_time(
+        gp_base_info["FirstPractice"]["date"], gp_base_info["FirstPractice"]["time"]
+    )
+
+    return gp_info
 
 
 def get_event_time(event_date, event_time):
     date_time_format = "%Y-%m-%d %H:%M:%S"
-    date_time_string = " ".join([event_date, event_time])
+    date_time_string = " ".join([event_date, event_time[:-1]])
     event_date_time = datetime.strptime(date_time_string, date_time_format)
     event_date_time = event_date_time.replace(tzinfo=tz.UTC)
     return event_date_time
 
 
-def get_countdown(event_date_time):
-    current_date = datetime.today()
+def get_countdown(event_date_time, current_date):
     current_date = current_date.replace(tzinfo=tz.gettz())
+    days_to_event = event_date_time.date() - current_date.date()
+    time_to_event = event_date_time - current_date
+    return days_to_event, time_to_event
 
-    days_to_race = event_date_time.date() - current_date.date()
-    time_to_race = event_date_time - current_date
-    return days_to_race.days, time_to_race
-
-
-def get_gp_events(race_json):
-    events = []
 
 @click.command()
-@click.argument("event", nargs=-1)
+@click.argument("event")
 @click.option("-o", "--omit", default=0)
 @click.option("-s", "--schedule", is_flag=True, default=False)
 @click.option("-d", "--days", "countdown", flag_value="days")
 @click.option("-c", "--countdown", "countdown", flag_value="countdown")
 def f1next(event, omit, schedule, countdown):
-    race_date_time = get_event_time(race_date, race_time)
-    days_to_race, time_to_race = get_countdown(race_date_time)
+    current_date = datetime.today()
 
-    if days_to_race >= omit:
-        echo_intro()
-        if countdown == "days":
-            echo_countdown_days(days_to_race)
-        elif countdown == "countdown":
-            echo_countdown_time(time_to_race)
-        elif countdown is None:
-            echo_countdown(days_to_race, time_to_race)
+    gp_info = parse_gp_info(download_gp_info())
 
-        click.echo()
+    echo_intro(gp_info)
 
-def echo_intro():
+    if event == "race":
+        click.echo("The race will start in", nl=False)
+        days_to_event, time_to_event = get_countdown(gp_info["race"], current_date)
+        echo_countdown_time(time_to_event)
+
+    # if days_to_race >= omit:
+    #     echo_intro()
+    #     if countdown == "days":
+    #         echo_countdown_days(days_to_race)
+    #     elif countdown == "countdown":
+    #         echo_countdown_time(time_to_race)
+    #     elif countdown is None:
+    #         echo_countdown(days_to_race, time_to_race)
+
+    #     click.echo()
+
+def echo_intro(gp_info):
     click.echo("The next ", nl=False)
     click.secho("Formula 1 ", fg="bright_red", nl=False)
-    click.echo("race is ", nl=False)
+    click.echo("weekend is ", nl=False)
     click.echo("the ", nl=False)
-    click.secho(race_name, fg="bright_green", nl=False)
-    click.echo(" race ", nl=False)
+    click.secho(gp_info["name"], fg="bright_green")
 
+def echo_event(gp_info, event_name):
+    pass
 
 def echo_date(race_date_time):
     click.echo(race_date_time.date().strftime("on %B %d at %I:%M %p"), nl=False)
@@ -92,12 +112,12 @@ def echo_countdown_days(days_to_race):
         click.echo("in " + str(days_to_race) + " days ", nl=False)
 
 
-def echo_countdown_time(time_to_race):
-    days = time_to_race.days
-    hours = floor(time_to_race.seconds / (60 * 60))
-    minutes = ceil((time_to_race.seconds / 60) % 60)
+def echo_countdown_time(time_to_event):
+    days = time_to_event.days
+    hours = floor(time_to_event.seconds / (60 * 60))
+    minutes = ceil((time_to_event.seconds / 60) % 60)
     if days == 1:
         click.echo("in 1 day,", nl=False)
     elif days > 1:
         click.echo(" in {} days,".format(days), nl=False)
-    click.echo(" {} hours and {} minutes".format(hours, minutes), nl=False)
+    click.echo(" {} hours and {} minutes".format(hours, minutes))
