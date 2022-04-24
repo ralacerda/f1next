166ff6bc83b21ef931d83eb9f8c8ade1c51b7353
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-04-23 23:52:46 -0300

    Refactoring echo_countdown function

--- f1next.py
+++ f1next.py
@@ -17,6 +17,8 @@ events = {
     "Sprint": "Sprint Race",
 }
 
+current_datetime = datetime.now(tz.gettz())
+
 
 def download_gp_info():
     cache_dir = appdirs.user_cache_dir("f1next", "f1next")
@@ -47,10 +49,7 @@ def parse_gp_info(json):
 
     gp_info["events"] = dict(sorted(gp_info["events"].items(), key=lambda v: v[1]))
 
-    print(gp_info)
-
     return gp_info
-
 
 def get_event_time(event_date, event_time):
     date_time_format = "%Y-%m-%d %H:%M:%S"
@@ -59,46 +58,17 @@ def get_event_time(event_date, event_time):
     event_date_time = event_date_time.replace(tzinfo=tz.UTC)
     return event_date_time
 
-
-def get_countdown(event_date_time, current_date):
-    current_date = current_date.replace(tzinfo=tz.gettz())
-    days_to_event = event_date_time.date() - current_date.date()
-    time_to_event = event_date_time - current_date
-    return days_to_event, time_to_event
-
-
 @click.command()
 @click.argument("event")
 @click.option("-s", "--schedule", is_flag=True, default=False)
-@click.option("-d", "--days", "countdown", flag_value="days")
-@click.option("-c", "--countdown", "countdown", flag_value="countdown")
-def f1next(event, schedule, countdown):
-    current_date = datetime.today()
-
+def f1next(event, schedule):
     gp_info = parse_gp_info(download_gp_info())
 
     echo_intro(gp_info)
 
     if not schedule:
-        days_to_event, time_to_event = get_countdown(
-            gp_info["events"]["Race"], current_date
-        )
-
-        click.echo("The race will start in", nl=False)
-
-        echo_countdown_time(time_to_event)
-
-    # if days_to_race >= omit:
-    #     echo_intro()
-    #     if countdown == "days":
-    #         echo_countdown_days(days_to_race)
-    #     elif countdown == "countdown":
-    #         echo_countdown_time(time_to_race)
-    #     elif countdown is None:
-    #         echo_countdown(days_to_race, time_to_race)
-
-    #     click.echo()
-
+        for event in gp_info["events"].keys():
+            echo_countdown(gp_info, event)
 
 def echo_intro(gp_info):
     click.echo("The next ", nl=False)
@@ -106,38 +76,32 @@ def echo_intro(gp_info):
     click.echo("weekend is ", nl=False)
     click.echo("the ", nl=False)
     click.secho(gp_info["name"], fg="bright_green")
-
-
-def echo_event(gp_info, event_name):
-    pass
+    # TODO Include country flag
 
 
 def echo_date(race_date_time):
     click.echo(race_date_time.date().strftime("on %B %d at %I:%M %p"), nl=False)
 
 
-def echo_countdown(days_to_race, time_to_race):
-    if days_to_race >= 1:
-        echo_countdown_days(days_to_race)
-    else:
-        echo_countdown_time(time_to_race)
+def echo_countdown(gp_info, event_name):
+    event_datetime = gp_info["events"][event_name]
+    time_left = event_datetime - current_datetime
+    hours = floor(time_left.seconds / (60 * 60))
+    minutes = ceil((time_left.seconds / 60) % 60)
 
+    if current_datetime < event_datetime:
+        click.echo("The ", nl=False)
+        click.echo(event_name, nl=False)
+        click.echo(" will start in ", nl=False)
+        if time_left.days > 1:
+            click.echo(time_left, nl=False)
+            click.echo("days")
+        elif time_left.days == 1:
+            click.echo("in 1 day,", nl=False)
+            click.echo("{} hours and {} minutes".format(hours, minutes))
+        elif time_left.days == 0:
+            click.echo("{} hours and {} minutes".format(hours, minutes))
 
-def echo_countdown_days(days_to_race):
-    if days_to_race == 0:
-        click.echo("today! ", nl=False)
-    elif days_to_race == 1:
-        click.echo("tomorrow ", nl=False)
-    elif days_to_race > 1:
-        click.echo("in " + str(days_to_race) + " days ", nl=False)
-
-
-def echo_countdown_time(time_to_event):
-    days = time_to_event.days
-    hours = floor(time_to_event.seconds / (60 * 60))
-    minutes = ceil((time_to_event.seconds / 60) % 60)
-    if days == 1:
-        click.echo("in 1 day,", nl=False)
-    elif days > 1:
-        click.echo(" in {} days,".format(days), nl=False)
-    click.echo(" {} hours and {} minutes".format(hours, minutes))
+# TODO: Schedule should print the full schedule color coded
+# If the event is in the past, countdown should print it in red
+# and include information about when it started
