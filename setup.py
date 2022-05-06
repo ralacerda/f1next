2ac8507d15f7a164aa5a82eebd2a26f50922bdb5
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-05-06 12:05:04 -0300

    Updated setup.py for PyPI publishing
    Based on the tutorial:
    https://realpython.com/pypi-publish-python-package/

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
+import pathlib
 from setuptools import setup
+
+# This configuration file is based on:
+# https://realpython.com/pypi-publish-python-package/
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="f1next",
     version="0.1.0",
+    license="MIT",
+    author="Renato Lacerda",
+    author_email="renato.ac.lacerda@gmail.com",
     py_modules=["f1next"],
+    description="Simple python script to get information about the next Formula 1 Grand Pix",
+    long_description=README,
+    long_description_content_type="text/markdown",
     install_requires=["click", "requests-cache", "appdirs", "python-dateutil"],
+    url="https://github.com/ralacerda/f1next",
     entry_points={
         "console_scripts": [
             "f1next = f1next:f1next",
